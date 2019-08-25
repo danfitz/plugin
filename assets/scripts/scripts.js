e0f4751410ba3a97d9c8137da273c479450d8cdf
Dan Fitz <dan.fitz@pm.me>
2019-08-25 12:35:16 -0400

    Small tweaks for better UI and instructions

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,19 +4,19 @@ const app = {};
 // Initialize starting properties for app
 app.score = 0; // score for game
 app.currentTime = null; // current time running
-app.taskTime = 2; // in minutes
-app.shortBreakTime = 1; // in minutes
-app.longBreakTime = 5; // in minutes
+app.workBlock = { type: "work", time: 0.05  }; // in minutes
+app.shortBreakBlock = { type: "short break", time: 1 }; // in minutes
+app.longBreakBlock = { type: "long break", time: 5 }; // in minutes
 app.paddingTime = 1; // in minutes
 app.cycle = [
-    app.taskTime,
-    app.shortBreakTime,
-    app.taskTime,
-    app.shortBreakTime,
-    app.taskTime,
-    app.shortBreakTime,
-    app.taskTime,
-    app.longBreakTime
+    app.workBlock,
+    app.shortBreakBlock,
+    app.workBlock,
+    app.shortBreakBlock,
+    app.workBlock,
+    app.shortBreakBlock,
+    app.workBlock,
+    app.longBreakBlock
 ];
 app.gameStatus = "off"; // possible values: "off", "on", "warning", "depleting", "paused", "done"
 app.batteryIcons = [
@@ -50,7 +50,6 @@ app.batteryDepleting = function () {
 app.updateTimer = function(timerSelector, inputMilliseconds) {
     // Convert input time to positive number if negative
     let milliseconds = inputMilliseconds < 0 ? -inputMilliseconds : inputMilliseconds;
-    let formattedTime = inputMilliseconds < 0 ? "-" : "";
 
     // Extract seconds
     let seconds = (milliseconds / 1000) % 60;
@@ -60,7 +59,7 @@ app.updateTimer = function(timerSelector, inputMilliseconds) {
     let minutes = (milliseconds / 1000 - seconds) / 60;
 
     // Concatenate minutes and seconds into mm:ss format
-    formattedTime += minutes + ":" + seconds;
+    const formattedTime = minutes + ":" + seconds;
 
     // Insert/update app.currentTime on page
     $(timerSelector).text(formattedTime);
@@ -68,22 +67,72 @@ app.updateTimer = function(timerSelector, inputMilliseconds) {
 
 // Method that runs through one full unit of time
 app.startTimer = function() {
-    app.currentTime = app.cycle[0] * 60000; // converted to milliseconds
-    app.updateTimer(".timer1", app.currentTime); // Initialize start time
-    $(".timer2").text(`${app.cycle[1]}:00`);
-    $(".timer3").text(`${app.cycle[2]}:00`);
-    $(".timer4").text(`${app.cycle[3]}:00`);
-    
-    $(".gameStatus").text(""); // removes any messages
+    // Update app properties and content on page when new timer starts
+    app.gameStatus = "on"; // Update game status
 
-    // Move chosen time to end of cycle array
-    const timeToMoveToEnd = app.cycle.shift();
-    app.cycle.push(timeToMoveToEnd);
+    const targetBlock = app.cycle.shift(); // remove first element from cycle array
+    app.currentTime = targetBlock.time * 60000; // converted to milliseconds
+    $(".timerStatus").text(targetBlock.type); // Updates timer status
+    app.cycle.push(targetBlock); // push target block to end of cycle array
+
+    app.updateTimer(".timer1", app.currentTime); // Update main timer
+    $(".timer2").text(`${app.cycle[0].time}:00`); // Update second timer
+    $(".timer3").text(`${app.cycle[1].time}:00`); // Update third timer
+    $(".timer4").text(`${app.cycle[2].time}:00`); // Update fourth timer
+    
+    $(".instructions").addClass("visuallyHidden"); // hides any instructions
+
     
     // At a rapid interval:
-    // 1. Update current time
-    // 2. Update score
-    app.intervalId = setInterval(function() {
+    // UPDATE SCORE
+    // UPDATE CURRENT TIME
+    app.intervalId = setInterval(function() {    
+        // As long as timer is greater or equal to the negative value of padding time...
+        if (app.currentTime >= app.paddingTime * -60000) {
+            // Increase score and update value on page while score is positive
+            if (app.currentTime >= 0) {
+                app.score += 1;
+                $(".score").text(app.score);
+            // When timer is negative but within padding time,
+            // update game status to "warning" and display messages on page
+            } else {
+                app.gameStatus = "warning";
+                $(".instructions").removeClass("visuallyHidden");
+                $(".instructionsText").text(`You have ${app.paddingTime} minute before you start losing charge...`);
+                $(".timerStatus").text("Overtime");
+            }
+            
+        // Otherwise, user has reached "depleting" game status and begins losing score
+        } else {
+            // Update game status and instructions
+            app.gameStatus = "depleting";
+            $(".instructionsText").text("Tap in to stop losing points...");
+            // Update score
+            app.score -= 1;
+            $(".score").text(app.score);
+        };
+        
+        // Visual signals for game: battery icon, screen colours, etc.
+        if (app.currentTime % 500 === 0) {
+            if (app.gameStatus === "on") {
+                app.batteryCharging();
+                $(".activeGameContainer").removeClass("depletingAlert warningAlert");
+                $(".tapInButton").removeClass("keyboardButtonFlash");
+            } else if (app.gameStatus === "warning") {
+                $(".battery")
+                .removeClass("batteryCharging")
+                .addClass("batteryProblem");
+                
+                $(".activeGameContainer").addClass("warningAlert");
+                $(".tapInButton").addClass("keyboardButtonFlash");
+            } else if (app.gameStatus === "depleting") {
+                app.batteryDepleting();
+                $(".activeGameContainer")
+                .removeClass("warningAlert")
+                .addClass("depletingAlert");
+            }
+        }
+
         // Decrease timer
         app.currentTime -= 20;
         
@@ -91,61 +140,17 @@ app.startTimer = function() {
         if (app.currentTime % 1000 === 0) {
             app.updateTimer(".timer1", app.currentTime);
         }
-        
-        // Increase score and update value on page as long as time is positive
-        if (app.currentTime >= app.paddingTime * -60000) {
-            // 
-            if (app.currentTime >= 0) {
-                // When timer goes into padding time, flash screen yellow and make icon battery grey
-                app.score += 1;
-                $(".currentScore").text(app.score);
-            } else {
-                app.gameStatus = "warning";
-                $(".gameStatus").text("Time until point loss begins...");
-            }
-
-        // Otherwise, user is in overtime and score decreases
-        } else {
-            app.gameStatus = "depleting";
-            $(".gameStatus").text("Tap in to stop losing points...");
-            app.score -= 1;
-            $(".currentScore").text(app.score);
-        };
-
-        // Visual signal of game status: battery icon and screen
-        if (app.currentTime % 500 === 0) {
-            if (app.gameStatus === "on") {
-                app.batteryCharging();
-                $(".activeGameContainer").removeClass("depletingAlert tapInAlert");
-                $(".tapInButton").removeClass("keyboardButtonFlash");
-            } else if (app.gameStatus === "warning") {
-                $(".battery")
-                    .removeClass("batteryCharging")
-                    .addClass("batteryProblem");
-    
-                $(".activeGameContainer").addClass("tapInAlert");
-                $(".tapInButton").addClass("keyboardButtonFlash");
-            } else if (app.gameStatus === "depleting") {
-                app.batteryDepleting();
-                $(".activeGameContainer")
-                    .removeClass("tapInAlert")
-                    .addClass("depletingAlert");
-            }
-        }
     }, 20);
 };
 
 // Tap in button
-app.tapIn = function() {
-    $(".tapInButton").on("click", function(event) {
+app.nextPrompt = function() {
+    $(".nextButton").on("click", function(event) {
         event.preventDefault();
         
         clearTimeout(app.intervalId);
-        app.gameStatus = "on";
         app.startTimer();
-        app.batteryCharging();
-        
-        //
+        // app.batteryCharging();
     });
 };
 
@@ -169,8 +174,8 @@ app.startGame = function() {
         
         // Starts game when plug has been plugged in
         setTimeout(function() {
+            $(".batteryScoreboard").css("opacity", "1");
             app.startTimer();
-            app.gameStatus = "on";
         }, 1900);
         
     });
@@ -179,7 +184,7 @@ app.startGame = function() {
 // App init method
 app.init = function() {
     this.startGame();
-    this.tapIn();
+    this.nextPrompt();
 };   
 
 // Start app
