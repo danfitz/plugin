f61ba5e256f600decce6346ceacd3e3eccfe1b96
Dan Fitz <dan.fitz@pm.me>
2019-08-25 15:54:51 -0400

    Successfully added pause AND shut down buttons!

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -18,7 +18,8 @@ app.cycle = [
     app.workBlock,
     app.longBreakBlock
 ];
-app.gameStatus = "off"; // possible values: "off", "on", "warning", "depleting", "paused", "done"
+app.gameStatus = "off"; // possible values: "off", "on", "warning", "depleting", "done"
+app.paused = false;
 app.batteryIcons = [
     "fa-battery-empty",
     "fa-battery-quarter",
@@ -92,62 +93,68 @@ app.startTimer = function() {
     // At a rapid interval:
     // UPDATE SCORE
     // UPDATE CURRENT TIME
-    app.intervalId = setInterval(function() {    
-        // As long as timer is greater or equal to the negative value of padding time...
-        if (app.currentTime >= app.paddingTime * -60000) {
-            // Increase score and update value on page while score is positive
-            if (app.currentTime >= 0) {
-                app.score += 1;
-                $(".score").text(app.score);
-                // When timer is negative but within padding time,
-            // update game status to "warning" and display messages on page
-        } else {
-                app.gameStatus = "warning";
-                $(".instructions").removeClass("visuallyHidden");
-                $(".timerStatus").text("Overtime");
+    app.intervalId = setInterval(function() {
+        // Only execute if game isn't paused...
+        if (!app.paused) {
+            // As long as timer is greater or equal to the negative value of padding time...
+            if (app.currentTime >= app.paddingTime * -60000) {
+                // Increase score and update value on page while score is positive
+                if (app.currentTime >= 0) {
+                    app.score += 1;
+                    $(".score").text(app.score);
+                    // When timer is negative but within padding time,
+                // update game status to "warning" and display messages on page
+            } else {
+                if (app.gameStatus !== "warning") {
+                    app.gameStatus = "warning";
+                    $(".instructions").removeClass("visuallyHidden");
+                    $(".instructionsText").text(`${app.paddingTime} minute(s) until score drops...`);
+                    $(".timerStatus").text("Overtime");
+                }
             }
             
             // Otherwise, user has reached "depleting" game status and begins losing score
-        } else {
-            // Update game status and instructions
-            app.gameStatus = "depleting";
-            // Update score
-            app.score -= 1;
-            $(".score").text(app.score);
-        };
-        
-        // Visual signals for game: battery icon, screen colours, etc.
-        if (app.currentTime % 500 === 0) {
-            if (app.gameStatus === "on") {
-                app.batteryCharging();
-                $(".activeGameContainer").removeClass("depletingAlert warningAlert");
-                $(".nextButton").removeClass("keyboardButtonFlash");
-            } else if (app.gameStatus === "warning") {
-                $(".battery")
-                    .removeClass("batteryCharging")
-                    .addClass("batteryProblem");
-
-                $(".instructionsText").text(`You have ${app.paddingTime} minute before you start losing charge...`);
-                
-                $(".activeGameContainer").addClass("warningAlert");
-                $(".nextButton").addClass("keyboardButtonFlash");
-            } else if (app.gameStatus === "depleting") {
-                app.batteryDepleting();
-
-                $(".instructionsText").text("Tap in to stop losing points...");
-
-                $(".activeGameContainer")
-                    .removeClass("warningAlert")
-                    .addClass("depletingAlert");
+            } else {
+                // Update game status and instructions
+                if (app.gameStatus !== "depleting") {
+                    app.gameStatus = "depleting";
+                    $(".instructionsText").text("Start next timer to stop losing points...");
+                }
+                // Update score
+                app.score -= 1;
+                $(".score").text(app.score);
+            };
+            
+            // Visual signals for game: battery icon, screen colours, etc.
+            if (app.currentTime % 500 === 0) {
+                if (app.gameStatus === "on") {
+                    app.batteryCharging();
+    
+                    $(".activeGameContainer").removeClass("depletingAlert warningAlert");
+                    $(".nextButton").removeClass("keyboardButtonFlash");
+                } else if (app.gameStatus === "warning") {
+                    $(".battery")
+                        .removeClass("batteryCharging")
+                        .addClass("batteryProblem");
+    
+                    $(".activeGameContainer").addClass("warningAlert");
+                    $(".nextButton").addClass("keyboardButtonFlash");
+                } else if (app.gameStatus === "depleting") {
+                    app.batteryDepleting();
+    
+                    $(".activeGameContainer")
+                        .removeClass("warningAlert")
+                        .addClass("depletingAlert");
+                }
             }
-        }
-        
-        // Decrease timer
-        app.currentTime -= 50;
-        
-        // Update time on page exactly every 1 second
-        if (app.currentTime % 1000 === 0) {
-            app.updateTimer(".timer1 .timerNum", app.currentTime);
+            
+            // Decrease timer
+            app.currentTime -= 50;
+            
+            // Update time on page exactly every 1 second
+            if (app.currentTime % 1000 === 0) {
+                app.updateTimer(".timer1 .timerNum", app.currentTime);
+            }
         }
     }, 50);
 };
@@ -179,8 +186,15 @@ app.buyTimePrompt = function() {
             app.score -= app.buyTimeCost;
             app.currentTime += 60000 * 5;
             app.updateTimer(".timer1 .timerNum", Math.round(app.currentTime) - Math.round(app.currentTime) % 1000);
-        }
-    })
+
+            if (app.currentTime >= 0) {
+                $(".instructions").addClass("visuallyHidden");
+            }
+        } else {
+            $(".instructions").removeClass("visuallyHidden");
+            $(".instructionsText").text("Not enough points to buy time");
+        };
+    });
 
     // For up arrow key event
     $(document).on("keydown", function(event) {
@@ -189,10 +203,76 @@ app.buyTimePrompt = function() {
                 app.score -= app.buyTimeCost;
                 app.currentTime += 60000 * 5;
                 app.updateTimer(".timer1 .timerNum", Math.round(app.currentTime) - Math.round(app.currentTime) % 1000);
-            }   
-        }
-    })
+
+                if (app.currentTime >= 0) {
+                    $(".instructions").addClass("visuallyHidden");
+                }
+            } else {
+                $(".instructions").removeClass("visuallyHidden");
+                $(".instructionsText").text("Not enough points to buy time");
+            };
+        };
+    });
+};
+
+app.pausePrompt = function() {
+    $(".pauseButton").on("click", function() {
+        if (!app.paused) {
+            app.paused = true;
+            $(".pauseButton .keyDescription").text("Unpause");
+            $(".pauseButton").addClass("keyboardButtonFlash");
+        } else {
+            app.paused = false;
+            $(".pauseButton .keyDescription").text("Pause");
+            $(".pauseButton").removeClass("keyboardButtonFlash");
+        };
+    });
+
+    $(document).on("keydown", function(event) {
+        if (event.keyCode === 80) {
+            if (!app.paused) {
+                app.paused = true;
+                $(".pauseButton .keyDescription").text("Unpause");
+                $(".pauseButton").addClass("keyboardButtonFlash");
+            } else {
+                app.paused = false;
+                $(".pauseButton .keyDescription").text("Pause");
+                $(".pauseButton").removeClass("keyboardButtonFlash");
+            };
+        };
+    });
+};
+
+app.shutdownPrompt = function() {
+    $(".shutdownButton").on("click", function() {
+        if (confirm("Are you sure you want to shut down?")) {
+            app.gameStatus = "done";
+            clearInterval(app.intervalId);
+    
+            const shutdownHtml = `
+                <div.shutdownCTA>
+                    <h1 class="appTitle">Plugin</h1>
+                    <p>Your final score: ${app.score}</p>
+                    <a href="https://twitter.com/intent/tweet?text=I%20just%20got%20a%20score%20of%20${app.score}%20using%20the%20Plugin%20app%20to%20help%20me%20stay%20productive!&via=_danielfitz&url=https://plugin.danielfitz.com" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                    <button class="restartButton keyboardButton">
+                        <i class="fas fa-redo key" aria-hidden="true"></i>
+                        <span class="sr-only">Restart</span>
+                        <span class="keyDescription">Restart</span>
+                    </button>
+                </div>
+                `;
+
+            $("main").html(shutdownHtml);
+        };
+    });
 }
+
+app.restartPrompt = function() {
+    $("main").on("click", ".restartButton", function() {
+        console.log("restart");
+        location.reload();
+    });
+};
 
 // Event listener that starts the game
 app.startGame = function() {
@@ -216,10 +296,9 @@ app.startGame = function() {
         
         // Starts game when plug has been plugged in
         setTimeout(function() {
-            $(".batteryScoreboard").css("opacity", "1");
+            $(".batteryScoreboard").removeClass("visuallyHidden"); // unhides scoreboard
             app.startTimer();
-        }, 1900);
-        
+        }, 1900);     
     });
 };
 
@@ -228,6 +307,9 @@ app.init = function() {
     this.startGame();
     this.nextPrompt();
     this.buyTimePrompt();
+    this.pausePrompt();
+    this.shutdownPrompt();
+    this.restartPrompt();
 };   
 
 // Start app
